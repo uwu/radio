1607141ef1c4d6324c4754f88efd961907bfce64
redstonekasi <kasi@hanneskann.de>
2022-11-22 20:34:06 +0100

    [client] fix sizing

--- uwuradio-client/src/ui/App.tsx
+++ uwuradio-client/src/ui/App.tsx
@@ -4,7 +4,7 @@ import RightPane from "./RightPane"
 
 const App: Component = () => {
   return (
-    <div class="bg-black text-white grid grid-cols-2 h-screen p-10 children:p-5">
+    <div class="root bg-black text-white grid h-screen p-10 children:p-5">
       <LeftPane />
       <div class="border-l border-white">
 	      <RightPane />
