2a410bea6d3949531ea1239ceaded688620aca95
yellowsink <yellowsink@riseup.net>
2022-11-22 20:32:03 +0000

    [misc] formatting ig

--- uwuradio-client/src/ui/App.tsx
+++ uwuradio-client/src/ui/App.tsx
@@ -1,16 +1,16 @@
 import type { Component } from "solid-js";
 import LeftPane from "./LeftPane";
-import RightPane from "./RightPane"
+import RightPane from "./RightPane";
 
 const App: Component = () => {
-  return (
-    <div class="root bg-black text-white grid h-screen p-10 children:p-5">
-      <LeftPane />
-      <div class="border-l border-white">
-	      <RightPane />
-      </div>
-    </div>
-  );
+	return (
+		<div class="root bg-black text-white grid h-screen p-10 children:p-5">
+			<LeftPane />
+			<div class="border-l border-white">
+				<RightPane />
+			</div>
+		</div>
+	);
 };
 
 export default App;
--- uwuradio-client/src/ui/RightPane.tsx
+++ uwuradio-client/src/ui/RightPane.tsx
@@ -1,19 +1,16 @@
 import { createMemo } from "solid-js";
-import { clientInstance } from "../syncClient"
+import { clientInstance } from "../syncClient";
 
 const quotes = createMemo(() => clientInstance.submitters.get(clientInstance.currentSong?.submitter)?.quotes);
 
-const randomQuote = createMemo(() =>
-	quotes() ? quotes()[~~(Math.random() * quotes().length)] : undefined)
+const randomQuote = createMemo(() => (quotes() ? quotes()[~~(Math.random() * quotes().length)] : undefined));
 
 export default () => (
-  <div>
-	  The right pane is a work in progress.
-	  <br />
-	  The current song was submitted by {clientInstance.currentSong?.submitter}.
-	  
-	  <br />
-
-	  {randomQuote()}
-  </div>
+	<div>
+		The right pane is a work in progress.
+		<br />
+		The current song was submitted by {clientInstance.currentSong?.submitter}.
+		<br />
+		{randomQuote()}
+	</div>
 );
