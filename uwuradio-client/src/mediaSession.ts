6a99ac7fabc8cbf724bb40933f43009a0ff4fb8a
redstonekasi <kasi@hanneskann.de>
2022-11-25 08:37:14 +0100

    [client] some browsers don't support the media session api

--- uwuradio-client/src/mediaSession.ts
+++ uwuradio-client/src/mediaSession.ts
@@ -13,25 +13,27 @@ export function setupMediaSession() {
   if (initalized) return;
   initalized = true;
 
-  const client = getClient();
-  audio.play();
+  if ("mediaSession" in navigator) {
+    const client = getClient();
+    audio.play();
 
-  watchEffect(() => {
-    navigator.mediaSession.metadata = new MediaMetadata({
-      title: client.currentSong?.name,
-      artist: client.currentSong?.artist,
-      album: client.currentSong?.album,
-      artwork: client.currentSong?.artUrl !== undefined ? [{ src: client.currentSong?.artUrl! }] : [],
-    });
-  });
-  
-  // Seperate effect since this is called every second
-  watchEffect(() => {
-    if (!isNaN(seek.value ?? 0)) {
-      navigator.mediaSession.setPositionState({
-        position: seek.value,
-        duration: getDuration(),
+    watchEffect(() => {
+      navigator.mediaSession.metadata = new MediaMetadata({
+        title: client.currentSong?.name,
+        artist: client.currentSong?.artist,
+        album: client.currentSong?.album,
+        artwork: client.currentSong?.artUrl !== undefined ? [{ src: client.currentSong?.artUrl! }] : [],
       });
-    }
-  });
+    });
+    
+    // Seperate effect since this is called every second
+    watchEffect(() => {
+      if (!isNaN(seek.value ?? 0)) {
+        navigator.mediaSession.setPositionState({
+          position: seek.value,
+          duration: getDuration(),
+        });
+      }
+    });
+  }
 }
