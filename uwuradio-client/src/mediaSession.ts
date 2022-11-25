889afbe12cc2f3a60ea9501869dfaeaae9f1a3a2
redstonekasi <kasi@hanneskann.de>
2022-11-26 00:39:18 +0100

    [client] polyfill media session api if it's missing

--- uwuradio-client/src/mediaSession.ts
+++ uwuradio-client/src/mediaSession.ts
@@ -13,28 +13,47 @@ export function setupMediaSession() {
   if (initalized) return;
   initalized = true;
 
-  if ("mediaSession" in navigator) {
-    const client = getClient();
-    audio.play();
+  const client = getClient();
+  audio.play();
 
-    watchEffect(() => {
-      navigator.mediaSession.metadata = new MediaMetadata({
-        title: client.currentSong?.name,
-        artist: client.currentSong?.artist,
-        album: client.currentSong?.album,
-        artwork:
-          client.currentSong?.artUrl !== undefined ? [{ src: client.currentSong?.artUrl! }] : [],
-      });
-    });
+  // Polyfill Media Session API so it can still be a data source even if it
+  // isn't supported by the browser.
+  // @ts-ignore
+  navigator.mediaSession ??= {
+    setPositionState() {},
+  };
+  // @ts-ignore
+  window.MediaMetadata ??= class {
+    album?: string;
+    artist?: string;
+    artwork?: MediaImage[];
+    title?: string;
 
-    // Seperate effect since this is called every second
-    watchEffect(() => {
-      if (!isNaN(seek.value ?? 0)) {
-        navigator.mediaSession.setPositionState({
-          position: seek.value,
-          duration: getDuration(),
-        });
-      }
-    });
+    constructor(data: MediaMetadataInit) {
+      this.album = data.album ?? "";
+      this.artist = data.artist ?? "";
+      this.artwork = data.artwork ?? [];
+      this.title = data.title ?? "";
+    }
   }
+
+  watchEffect(() => {
+    navigator.mediaSession.metadata = new MediaMetadata({
+      title: client.currentSong?.name,
+      artist: client.currentSong?.artist,
+      album: client.currentSong?.album,
+      artwork:
+        client.currentSong?.artUrl !== undefined ? [{ src: client.currentSong?.artUrl! }] : [],
+    });
+  });
+
+  // Seperate effect since this is called every second
+  watchEffect(() => {
+    if (!isNaN(seek.value ?? 0)) {
+      navigator.mediaSession.setPositionState({
+        position: seek.value,
+        duration: getDuration(),
+      });
+    }
+  });
 }
