24e50a8e483a09649b44b659826726877803080e
redstonekasi <kasi@hanneskann.de>
2022-12-13 23:07:37 +0100

    [clients/web] hopefully fix IOS x2

--- clients/web/src/main.ts
+++ clients/web/src/main.ts
@@ -6,9 +6,5 @@ import "@unocss/reset/tailwind.css";
 import "uno.css";
 import "./assets/styles.css";
 
-// hopefully fix IOS
-// @ts-ignore
-window.AudioContext = window.AudioContext || window.webkitAudioContext;
-
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 createApp(App).mount("#app");
