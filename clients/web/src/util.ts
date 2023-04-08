c4cfca9e711868a634804c7b310511315548970f
yellowsink <yellowsink@riseup.net>
2023-04-08 17:53:44 +0100

    [clients/web] improve accuracy of time syncing

--- clients/web/src/util.ts
+++ clients/web/src/util.ts
@@ -1,8 +1,14 @@
 import { serverUrl } from "./constants";
 
 export let timeOffset = 0;
-export const timePromise = fetch(new URL("/api/time", serverUrl))
-  .then((r) => r.json())
-  .then((serverTime) => void (timeOffset = serverTime - ~~(Date.now() / 1000)));
+export const timePromise = (async () => {
+  const timeBefore = ~~(Date.now() / 1000);
+
+  const serverTime = await fetch(new URL("/api/time", serverUrl)).then((r) => r.json());
+
+  const timeNow = ~~(Date.now() / 1000);
+
+  timeOffset = serverTime - ~~((timeNow - timeBefore) / 2) - timeNow;
+})();
 
 export const currentTimestamp = () => ~~(Date.now() / 1000) + timeOffset;
