57e8f5eab4158e1274b9bff848f96bfe458863a2
yellowsink <yellowsink@riseup.net>
2023-10-11 20:31:58 +0100

    [server] silly diagnostic tool that downloads all songs

--- UwuRadio.Server/Program.cs
+++ UwuRadio.Server/Program.cs
@@ -18,6 +18,32 @@ builder.Services.AddSingleton<CoordinatorService>();
 
 var app = builder.Build();
 
+// UwuRadio.Server --cache-all
+// debug tool that downloads all songs in ingest.
+#if DEBUG
+if (args.Contains("--cache-all"))
+{
+	Helpers.Log("debug cache-all", "Starting full ingest cache...");
+	var dataSrv = app.Services.GetService<DataService>()!;
+	var dlSrv   = app.Services.GetService<DownloadService>()!;
+
+	foreach (var song in dataSrv.Songs) dlSrv.EnsureDownloaded(song);
+
+	foreach (var (song, idx) in dataSrv.Songs.Select((s, idx) => (s, idx)))
+	{
+		while (!dlSrv.IsDownloaded(song) && !dlSrv.IsBlacklisted(song))
+			// lol this makes the entire program wrapped in an async state machine in debug builds :)
+			await Task.Delay(100);
+		
+		Helpers.Log("debug cache-all", $"downloaded: {idx} of {dataSrv.Songs.Length}");
+	}
+	
+	Helpers.Log("debug cache-all", "successfully downloaded all songs");
+	
+	return;
+}
+#endif
+
 // https://stackoverflow.com/a/66240442/8388655
 app.UseCors(cors => cors.AllowAnyHeader().AllowAnyMethod().SetIsOriginAllowed(_ => true).AllowCredentials());
 
@@ -30,6 +56,7 @@ app.UseEndpoints(endpoints =>
 });
 
 // start the services way before they're technically needed so that it starts downloading instantly
+// to clarify: without this, the server would sit until someone tried to listen, then cold-start.
 app.Services.GetService<CoordinatorService>();
 
 Helpers.Log(null, "Kickstarted services successfully, starting web server now");
