d3e210c6fb386dc72feb0374326dfc6f492f452a
yellowsink <yellowsink@riseup.net>
2023-10-12 20:36:55 +0100

    Revert "[clients > web] switch from buffers to HTML5 audio"
    This reverts commit 263e312181df43daed3bef617a6eea256df5e4fd.

--- clients/web/src/audio.ts
+++ clients/web/src/audio.ts
@@ -6,12 +6,12 @@ import { currentTimestamp } from "./util";
 export const audioCtx = new AudioContext();
 export const audioAnalyser = new AnalyserNode(audioCtx);
 const audioGain = audioCtx.createGain();
-let audioSource: MediaElementAudioSourceNode;
+let audioSource: AudioBufferSourceNode;
 
 let startTime: number;
 let startSeek: number;
 
-const songs: Record<string, HTMLAudioElement> = {};
+const songs: Record<string, Promise<AudioBuffer>> = {};
 
 export const history = reactive<Array<Song>>([]);
 export const volume = ref<number>(JSON.parse(localStorage.getItem("volume") ?? "1"));
@@ -35,42 +35,32 @@ setInterval(
 
 export const seekTo = (seek: number) => {
   startSeek = seek;
-  if (audioSource) audioSource.mediaElement.currentTime = seek;
+  audioSource?.start(0, seek);
 };
 
-export const getDuration = () => audioSource?.mediaElement?.duration ?? 0;
+export const getDuration = () => audioSource?.buffer?.duration ?? 0;
 export const prettyDuration = () => prettyFormatTime(getDuration());
 
 async function loadAudio(url: string) {
-  const audio = new Audio(url);
-  audio.crossOrigin = "anonymous";
-
-  await new Promise((res, rej) => {
-    audio.onloadedmetadata = res; // full load
-    audio.onerror = rej; // error handling :)
-  });
-
-  // prevent a potential memory leak from keeping all the audio in ram
-  audio.onended = () => {
-    delete songs[url];
-  };
-
-  return audio;
+  const response = await fetch(url);
+  const arrayBuffer = await response.arrayBuffer();
+  const audioBuffer = await audioCtx.decodeAudioData(arrayBuffer);
+  return audioBuffer;
 }
 
 export async function preload(url: string) {
-  return (songs[url] ??= await loadAudio(url));
+  return (songs[url] ??= loadAudio(url));
 }
 
 export async function play(song: Song, seek: number) {
   const then = currentTimestamp();
   setupMediaSession();
-  audioSource?.mediaElement.pause();
+  audioSource?.stop();
 
   const url = song.dlUrl!;
 
-  audioSource = new MediaElementAudioSourceNode(audioCtx, {
-    mediaElement: await preload(url),
+  audioSource = new AudioBufferSourceNode(audioCtx, {
+    buffer: await (songs[url] ?? preload(url)),
   });
 
   audioSource.connect(audioAnalyser).connect(audioGain).connect(audioCtx.destination);
@@ -78,26 +68,7 @@ export async function play(song: Song, seek: number) {
   seek = seek + currentTimestamp() - then;
   startTime = audioCtx.currentTime;
   startSeek = seek;
-
-  audioSource.mediaElement.currentTime = seek;
-
-  // HTML5 audio has this problem where we seek before the audio is actually loaded and it just seeks to 0
-  // to force our way around it, we say that, if the difference between the starting seek and the current time > 2,
-  // and the audio is not yet fully loaded, then we should re-set the seek, and wait for network progress, then retry
-  // eventually, the seek will successfully set within range, and we can exit this loop and play()
-  while (
-    Math.abs(seek - audioSource.mediaElement.currentTime) > 2 &&
-    audioSource.mediaElement.networkState !== 1
-  ) {
-    await new Promise((res, rej) => {
-      audioSource.mediaElement.onprogress = res;
-      audioSource.mediaElement.onerror = rej;
-    });
-
-    audioSource.mediaElement.currentTime = seek;
-  }
-
-  audioSource.mediaElement.play();
+  audioSource.start(0, seek);
 
   // not having .at(-1) makes me sad
   if (history[history.length - 1] !== song) history.push(song);
