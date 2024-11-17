21708022c6ef181f0b545559cdc7c20d8dc83bd8
yellowsink <yellowsink@riseup.net>
2024-11-17 21:03:47 +0000

    [server] notably improve normalisation

--- UwuRadio.Server/DSP.cs
+++ UwuRadio.Server/DSP.cs
@@ -1,4 +1,6 @@
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
 using System.Text.RegularExpressions;
 
 namespace UwuRadio.Server;
@@ -9,74 +11,89 @@ namespace UwuRadio.Server;
 // ReSharper disable once InconsistentNaming
 public static class DSP
 {
-	public record struct LoudnessMeasurement(
-		// LUFS
-		double IntegratedLoudness, double IntegratedThreshold,
-		// LU
-		double LoudnessRange,
-		// dBFS
-		double TruePeak);
-	
-	private static readonly Regex LoudnessMeasurementRegex = new(@"I:\s*(.*) LUFS\s*Threshold:\s*(.*) LUFS[\s\S]*LRA:\s*(.*) LU[\s\S]*Peak:\s*(.*) dBFS");
-	
-	public static async Task<LoudnessMeasurement> MeasureLoudness(string path)
+	[SuppressMessage("ReSharper", "InconsistentNaming")]
+	public record struct RawLoudnorm(
+		string input_i,
+		string input_tp,
+		string input_lra,
+		string input_thresh,
+		string target_offset);
+
+	[SuppressMessage("ReSharper", "InconsistentNaming")]
+	public record struct Loudnorm(double I, double TP, double LRA, double Thresh, double Oset)
+	{
+		public Loudnorm(RawLoudnorm rw) : this(
+											   double.Parse(rw.input_i),
+											   double.Parse(rw.input_tp),
+											   double.Parse(rw.input_lra),
+											   double.Parse(rw.input_thresh),
+											   double.Parse(rw.target_offset))
+		{
+		}
+	}
+
+	private static readonly Regex JsonRegex = new(@"\[Parsed_loudnorm.*]\s*(\{[\s\S]*})");
+
+	public static async Task<Loudnorm> MeasureLoudness(string path)
 	{
 		var startOptions = new ProcessStartInfo("ffmpeg")
 		{
 			ArgumentList =
 			{
+				"-hide_banner",
+				"-nostats",
+				"-loglevel",
+				"info",
 				"-nostdin",
 				"-i",
 				path,
-				"-filter:a",
-				"ebur128=peak=true",
+				"-af",
+				$"loudnorm=I={Constants.C.AudioNormIntegrated}:TP={Constants.C.AudioNormMaxClip}:print_format=json",
 				"-f",
 				"null",
 				"-"
 			},
-			WorkingDirectory       = Path.GetTempPath(),
-			RedirectStandardError  = true
+			WorkingDirectory      = Path.GetTempPath(),
+			RedirectStandardError = true
 		};
 
 		var process = Process.Start(startOptions);
 		var stdErr  = await process!.StandardError.ReadToEndAsync();
-		var match   = LoudnessMeasurementRegex.Match(stdErr);
-		
-		var lufs  = double.Parse(match.Groups[1].Value);
-		var thres = double.Parse(match.Groups[2].Value);
-		var lra   = double.Parse(match.Groups[3].Value);
-		var peak  = double.Parse(match.Groups[4].Value);
-		//Console.Error.WriteLine(stdErr);
-		//Console.Error.WriteLine($"measurement: {lufs} LUFS, {thres} threshold, {lra} LU range, {peak} dbFS peak");
-		
-		return new LoudnessMeasurement(lufs, thres, lra, peak);
+
+		var match = JsonRegex.Match(stdErr);
+
+		return new Loudnorm(JsonSerializer.Deserialize<RawLoudnorm>(match.Groups[1].ValueSpan));
 	}
 
-	public static async Task Normalize(string inPath, string outPath, LoudnessMeasurement measurement)
+	public static async Task Normalize(string inPath, string outPath, Loudnorm measurement)
 	{
-		var lufsLoudnessChange = Constants.C.AudioNormIntegrated - measurement.IntegratedLoudness;
-		var clampedLoudnessChange = Math.Min(lufsLoudnessChange, -(measurement.TruePeak - Constants.C.AudioNormMaxClip));
-		
-		//Console.Error.WriteLine($"wanted adjustment: {lufsLoudnessChange} (to hit target {Constants.C.AudioNormIntegrated} from measurment {measurement.IntegratedLoudness}");
-		//Console.Error.WriteLine($"would result in a true peak level of {lufsLoudnessChange + measurement.TruePeak}");
-		//Console.Error.WriteLine($"so actual adjustment: {clampedLoudnessChange} (max clip: {Constants.C.AudioNormMaxClip}db)");
-		
-		var args = new List<string> { "-nostdin", "-i", inPath, "-filter:a",
-			$"volume={clampedLoudnessChange}dB",
+		var args = new List<string>
+		{
+			"-nostdin",
+			"-i",
+			inPath,
+			"-af",
+			$"loudnorm=I={Constants.C.AudioNormIntegrated}:TP={Constants.C.AudioNormMaxClip}"
+		  + $":measured_I={measurement.I}:measured_TP={measurement.TP}"
+		  + $":measured_LRA={measurement.LRA}:measured_thresh={measurement.Thresh}"
+		  + $":offset={measurement.Oset}:linear=true",
 			"-f", Constants.C.AudioFormat
 		};
-		
+
 		if (!string.IsNullOrWhiteSpace(Constants.C.AudioQScale))
-			args.AddRange(new [] {"-q:a", Constants.C.AudioQScale});
-		
+			args.AddRange(new[] { "-q:a", Constants.C.AudioQScale });
+
 		if (!string.IsNullOrWhiteSpace(Constants.C.AudioBitrate))
-			args.AddRange(new [] {"-b:a", Constants.C.AudioBitrate});
-		
+			args.AddRange(new[] { "-b:a", Constants.C.AudioBitrate });
+
 		args.Add(outPath);
 
 		var psi = new ProcessStartInfo("ffmpeg") { RedirectStandardError = true };
 		foreach (var a in args) psi.ArgumentList.Add(a);
-		
-		await Process.Start(psi)!.WaitForExitAsync();
+
+		var ps = Process.Start(psi)!;
+		await ps.WaitForExitAsync();
+
+		var _stderr = await ps.StandardError.ReadToEndAsync();
 	}
 }
