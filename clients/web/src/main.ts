ce786b6345e9e7185ed1feaf74f8bfddb8d65f18
yellowsink <yellowsink@riseup.net>
2024-12-02 15:44:51 +0000

    [clients>web] see uwu/uwu.network 3b3eb5d

--- clients/web/src/main.ts
+++ clients/web/src/main.ts
@@ -6,5 +6,10 @@ import "@unocss/reset/tailwind.css";
 import "uno.css";
 import "./assets/styles.css";
 
+// dark favicon
+if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches)
+  document.head.querySelector("link")!.href = "https://uwu.network/favicon-white.ico";
+
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 createApp(App).mount("#app");
